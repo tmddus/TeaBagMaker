39afa29adda1c6c4c30c1ea9a5f47422257ccd3e
송승연 <wnzk413@gmail.com>
2019-05-21 11:13:02 +0900

    코드 구현 완료

--- TeaBagMakers/Form1.Designer.cs
+++ TeaBagMakers/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -47,6 +48,7 @@
             // 
             // timer1
             // 
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // comboBox1
@@ -56,10 +58,11 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(391, 23);
             this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(30, 182);
+            this.button1.Location = new System.Drawing.Point(30, 229);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(391, 43);
             this.button1.TabIndex = 2;
@@ -78,17 +81,29 @@
             this.label2.Text = "TeaBagMaker";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(433, 450);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,6 +116,7 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- TeaBagMakers/Form1.cs
+++ TeaBagMakers/Form1.cs
@@ -7,19 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace TeaBagMakers
 {
     public partial class Form1 : Form
     {
+        int CountNum = 0; 
+
         public Form1()
         {
             InitializeComponent();
         }
 
+
+        // 콤보 박스 데이터
+        string[] data = new string[] { "홍차", "녹차", "루이보스차", "국화차" };
+        int[] timedata = new int[] { 60, 180, 300, 120 };
+
+        string tea = "";
+
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            if (CountNum < 1)
+            {
+                this.timer1.Enabled = false;
+                this.label3.Text = "";
+                MessageBox.Show("티백을 건지세요!", "타임아웃", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                comboBox1.Focus();
+            }
+            else
+            {
+                CountNum--;
+                if (CountNum > 60) {
+                    this.label3.Text = CountNum / 60 + "분" + CountNum % 60 + "초";
+                }
+                else
+                {
+                    this.label3.Text = CountNum + "초";
+                }
+            }
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -28,6 +56,32 @@ namespace TeaBagMakers
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            this.timer1.Enabled = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {   
+            
+            this.label3.Text = timedata[this.comboBox1.SelectedIndex] / 60 + "분";
+            CountNum = timedata[this.comboBox1.SelectedIndex];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < data.Length; i++)
+            {
+                this.comboBox1.Items.Add(data[i]);
+            }
+            this.label3.Text = timedata[0]/60 + "분";
+            CountNum = timedata[0];
+            if(comboBox1.Items.Count > 0)
+            {
+                this.comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
